1b36ac306747876ec2b5f82f4a441ffbd57e2ebb
billietentacion05 <70089065+billietentacion05@users.noreply.github.com>
2020-08-23 13:03:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyDqcpaejBi8Y-OJchCqVsMIIDAppkDt6QY",
+  authDomain: "project-653e0.firebaseapp.com",
+  databaseURL: "https://project-653e0.firebaseio.com",
+  projectId: "project-653e0",
+  storageBucket: "project-653e0.appspot.com",
+  messagingSenderId: "304561679430",
+  appId: "1:304561679430:web:13d8d64a4bd8ac6f466e47",
+  measurementId: "G-L7NEJKQ6K0"
 };
-
      
  
 ////Do Not Edit Below This
